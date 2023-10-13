18d914b1ef3cabcc8102adfb99c51d6985d1561c
Alessandro Lamin <alessandro.lamin@ares2t.com>
2023-10-13 17:18:19 +0200

    Miglioramenti all'interfaccia Charge Advisor, fix allo slider di potenza assorbita, moglioramento dati raccolti dal campo, aggiunte nuove funzioni per il supporto di OCPP 2.0.1

--- custom_components/charge_advisor/charge_point_v201.py
+++ custom_components/charge_advisor/charge_point_v201.py
@@ -29,7 +29,7 @@ import homeassistant.helpers.config_validation as cv
 # Local packages
 # ----------------------------------------------------------------------------------------------------------------------
 
-from ocpp_central_system.ocpp_central_system.ComponentsV201.charging_station_2_0_1 import ChargingStationV201
+from ocpp_central_system.ComponentsV201.charging_station_2_0_1 import ChargingStationV201
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Local files
